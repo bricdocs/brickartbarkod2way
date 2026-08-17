2db5939261a1962bb7a4dd2a3e853474a8c90681
bricdocs <bricdocs.web@gmail.com>
2026-08-17 21:43:02 +0300

    Create barkod-data.js with barcode mappings
    Added external barcode database mapping for JANNERSTEN.

---
+++ barkod-data.js
@@ -0,0 +1,14 @@
+/***********************************************************
+ * HARİCİ JANNERSTEN BARKOD VERİTABANI (barkod-data.js)
+ * v5.20 Kararlı Motor Kayıtları
+ ***********************************************************/
+
+const JANNERSTEN_DECK_MAP = {
+    // CA Kartı
+    "I-D-K-D-I-D-I-G": "CA",
+    "I-D-K-D-I-G-I-G": "CA",
+
+    // C3 Kartı (Kesinleşen Kararlı İmza)
+    "D-I-D-I-D-I-D-I": "C3",
+    "I-D-I-D-I-D-I-D": "C3"
+};
